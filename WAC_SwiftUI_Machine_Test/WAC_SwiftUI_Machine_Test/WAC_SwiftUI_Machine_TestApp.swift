fa60c2ecda815b0b00831005205eba3f53b80311
Chandramohan <chandra@qaptive.co.in>
2023-08-17 23:14:38 +0530

    project created

---
+++ WAC_SwiftUI_Machine_Test/WAC_SwiftUI_Machine_Test/WAC_SwiftUI_Machine_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WAC_SwiftUI_Machine_TestApp.swift
+//  WAC_SwiftUI_Machine_Test
+//
+//  Created by Chandra Mohan on 17/08/23.
+//
+
+import SwiftUI
+
+@main
+struct WAC_SwiftUI_Machine_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
