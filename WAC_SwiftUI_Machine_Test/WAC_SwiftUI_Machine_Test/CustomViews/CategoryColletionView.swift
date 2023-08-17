9a9acb012618218c8d4de23065ff9022b7c6b690
Chandramohan <chandra@qaptive.co.in>
2023-08-18 03:06:16 +0530

    random colour for category image implemented

--- WAC_SwiftUI_Machine_Test/WAC_SwiftUI_Machine_Test/CustomViews/CategoryColletionView.swift
+++ WAC_SwiftUI_Machine_Test/WAC_SwiftUI_Machine_Test/CustomViews/CategoryColletionView.swift
@@ -28,15 +28,26 @@ struct CategoryCell: View {
     
     var body: some View {
         VStack {
-            KFImage(URL(string: categoryObject.imageURL ?? ""))
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 50, height: 50)
-                .background(Color.gray.opacity(0.3))
-                .clipShape(Circle())
+            ZStack{
+                Circle()
+                    .foregroundColor(randomColor()) // Use the randomColor function
+                    .frame(width: 60, height: 60)
+                KFImage(URL(string: categoryObject.imageURL ?? ""))
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 50, height: 50)
                 
+            }
+            
+            
             Text(categoryObject.name ?? "-")
                 .font(.headline)
         }
+    }
+    private func randomColor() -> Color {
+        let red = Double.random(in: 0...1)
+        let green = Double.random(in: 0...1)
+        let blue = Double.random(in: 0...1)
+        return Color(red: red, green: green, blue: blue).opacity(0.3)
     }
 }
